1dfda717c76af280cf23b51ae2650fac7bee6a9d
Pojjara <84350810+Pojjara@users.noreply.github.com>
2023-09-26 17:22:01 +0100

    Add files via upload

---
+++ TypingPractice/static/TypingPractice/script.js
@@ -0,0 +1,169 @@
+let newWordsFetched = false;
+function fetchNewWords(){
+    let min = document.querySelector('.min').value
+    let max = document.querySelector('.max').value
+    return fetch(`/words/${min}/${max}`)
+    .then(response => response.json());
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    var words = ''
+
+    fetchNewWords()
+    .then(data => {
+
+        // Handle the fetched data here
+        words = data['words']
+        console.log(words)
+        displayWords(words)
+        
+        return words
+    })
+    .then( words => {
+
+    
+        
+        rawText = ''
+        words.forEach(word => {
+
+            rawText += word + ' '
+            
+        });
+
+        text = ''
+        words.forEach(word => {
+
+            text += word + "_"
+
+        });
+        text = text.slice(0, -1)
+
+    let allChars = document.querySelectorAll('.characterSpan')
+    allChars[0].classList.add('current')
+
+
+    //Listen if the user clicked correct key/letter
+    var currentIndex = 0
+    var mistakes = 0
+    let mistakesP = document.querySelector('.mistakes')
+    mistakesP.innerHTML = 'Mistakes: ' + mistakes
+    
+    document.addEventListener('keypress', function(key){
+
+        var currentChar = allChars[currentIndex]
+        var previousChar = allChars[currentIndex - 1]
+        var nextChar = allChars[currentIndex + 1]
+
+
+        if(!nextChar){
+                
+                fetchNewWords()
+                    .then(data => {
+                    // Handle the fetched data here
+                    let words = data['words']
+                    console.log(words)
+                    displayWords(words)
+                    
+                    rawText = ''
+                    words.forEach(word => {
+                        rawText += word + ' '
+                    });
+            
+                    text = ''
+                    words.forEach(word => {
+                        text += word + "_"
+            });
+            text = text.slice(0, -1)
+            allChars = document.querySelectorAll('.characterSpan')
+            allChars[0].classList.add('current')
+            currentIndex = 0
+            mistakes = 0
+            mistakesP = document.querySelector('.mistakes')
+            mistakesP.innerHTML = 'Mistakes: ' + mistakes
+            })
+            
+        
+    }
+        if(isKeyPressCorrect(key,currentIndex)){
+
+            console.log('Correct!')
+            
+            if(currentIndex != 0){
+                currentChar.classList.remove('current')
+            }
+            else{
+                currentChar.classList.remove('current')
+            }
+            currentChar.classList.add('correct')
+            currentIndex++
+            currentChar = allChars[currentIndex]
+            currentChar.classList.add('current')
+
+            
+
+        }
+        else{
+            console.log('Wrong key pressed!')
+            console.log(`Letter at index ${currentIndex}: '${rawText[currentIndex]}' Key pressed : '${key['key']}'`)
+            currentChar.classList.add('mistake')
+            mistakes++
+            mistakesP.innerHTML = 'Mistakes: ' + mistakes
+        }
+
+
+    })
+})
+
+    function isKeyPressCorrect(key,index){
+
+        if(key['key'].toLowerCase() == rawText[index].toLowerCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    function displayWords(words){
+
+        document.querySelector('.text').innerHTML = ''
+        words.forEach((eachWord, i)=> {
+            let outerSpan = document.createElement('span')
+            outerSpan.classList.add('outerSpan')
+            let innerSpan = document.createElement('span')
+            innerSpan.classList.add('wordSpan')
+    
+            for (let i = 0; i < eachWord.length; i++) {
+                let characterSpan = document.createElement('span')
+                characterSpan.classList.add('characterSpan')
+                characterSpan.innerHTML = eachWord.charAt(i)
+                innerSpan.appendChild(characterSpan)
+              }
+    
+    
+            outerSpan.appendChild(innerSpan)
+    
+            //Add '_' after each word, except the last one
+            if(words.length != i+1){
+                let spaceSpan = document.createElement('span')
+                spaceSpan.innerHTML = '␣'
+                spaceSpan.classList.add('characterSpan')
+                spaceSpan.classList.add('space')
+                innerSpan.appendChild(spaceSpan)
+            }
+            
+            mainDiv = document.querySelector('.text')
+            mainDiv.appendChild(outerSpan)
+        });
+    }
+
+
+
+
+
+})
+
+
+
+
---
+++ TypingPractice/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+from json import dumps
+import random
+import os
+
+
+amountOfWords = 5
+
+def index(request):
+    
+
+    return render(request, "TypingPractice/index.html")
+
+
+def fetchWords(request,min,max):
+
+    listOfWords = []
+    
+
+    i = 0
+
+    while i < amountOfWords:
+        random_file = chooseRandomFile()
+
+        with open(f'TypingPractice/Dictionary/{random_file}', 'r') as f:
+            lines = f.readlines()
+            word = random.choice(lines).strip()
+            if len(word) > min and len(word) < max and word not in listOfWords and ' ' not in word:
+
+                listOfWords.append(word)
+                i += 1
+        
+
+    return JsonResponse({
+        'words': listOfWords
+    })
+        
+    
+
+def chooseRandomFile():
+    file_list = os.listdir('TypingPractice\Dictionary')
+    # Filter out only files (not directories)
+    file_list = [file for file in file_list if os.path.isfile(os.path.join('TypingPractice\Dictionary', file))]
+
+    if file_list:
+        # Choose a random file from the list
+        random_file = random.choice(file_list)
+        print(f"Randomly chosen file: {random_file}")
+        return random_file
+    else:
+        print("No files found in the directory.")
+        return None
